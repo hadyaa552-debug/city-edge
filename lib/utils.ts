deea8324cefff6ca777c4b0c0c3b3a36d7b5ab2d
hadyaa552-debug <hadyaa552@gmail.com>
2026-04-27 00:52:09 +0300

    pug

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getWhatsAppUrl(phone: string, message?: string): string {
+  const encoded = message ? encodeURIComponent(message) : ""
+  return `https://wa.me/${phone.replace(/\D/g, "")}${encoded ? `?text=${encoded}` : ""}`
+}
